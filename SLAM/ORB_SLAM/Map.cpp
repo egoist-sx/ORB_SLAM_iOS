dbd56ffd0249c3ccf7f81d16423ef71993bf6f39
Xin Sun <info@xinsun.io>
2016-11-25 16:15:11 +0800

    added liscence from original author

--- SLAM/ORB_SLAM/Map.cpp
+++ SLAM/ORB_SLAM/Map.cpp
@@ -1,10 +1,22 @@
-//
-//  Map.cpp
-//  ORB_SLAM_iOS
-//
-//  Created by Xin Sun on 14/10/2015.
-//  Copyright © 2015 Xin Sun. All rights reserved.
-//
+/**
+* This file is part of ORB-SLAM.
+*
+* Copyright (C) 2014 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
+* For more information see <http://webdiis.unizar.es/~raulmur/orbslam/>
+*
+* ORB-SLAM is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with ORB-SLAM. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "Map.hpp"
 
